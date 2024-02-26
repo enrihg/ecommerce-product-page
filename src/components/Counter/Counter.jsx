31fed6c7ed0246851d9025ba59a336cfe0d11a2a
enrihg <99612570+enrihg@users.noreply.github.com>
2024-02-26 20:10:15 -0300

    counter component added

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,22 @@
 import './Counter.css';
 import iconMinus from '../../assets/images/icon-minus.svg';
 import iconPlus from '../../assets/images/icon-plus.svg';
+import { useState } from 'react';
 
 function Counter() {
+    const [qty, setQty] = useState(0);
+    const add = () => {
+        setQty(qty + 1);
+    }
+    const substract = () => {
+        (qty > 0) ? setQty(qty - 1) : '';
+    }
+
     return (
         <div className='counter'>
-            <button className='button-minus'><img src={iconMinus} alt="minus"/></button>
-            <span>0</span>
-            <button className='button-plus'><img src={iconPlus} alt="plus"/></button>
+            <button onClick={substract} className='button-minus'><img src={iconMinus} alt="minus"/></button>
+            <span>{qty}</span>
+            <button onClick={add} className='button-plus'><img src={iconPlus} alt="plus"/></button>
         </div>
     )
 }
